d5a9fe71d29e6866339b3dfc89b2fcce4c557f7e
FRopero <fernan_rp@hotmail.com>
2020-12-10 13:55:45 +0100

    PhD Update

--- js/init.js
+++ js/init.js
@@ -539,10 +539,10 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["PhD Student", "Researcher"],
+			strings: ["PhD in Computer Science", "Scientific Researcher"],
 			loop: true,
 			startDelay: 2e3,
-			backDelay: 4e3
+			backDelay: 6e3
 		});
 }
 
